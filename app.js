646b72113d52c20107e663e5ff3e7e2fcb4a8e38
Your Name <rigelvicky@gmail.com>
2021-09-25 02:51:54 +0300

    Fully Responsive With Gsap Animation

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ navToggle.addEventListener("click", function() {
 
 
 
-
-
 // ********** fixed navbar ************
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
@@ -28,7 +26,7 @@ window.addEventListener('scroll', scrollHeader)
 
 
 
-// ********** ACCORDIONr ************
+// ********** ACCORDION ************
 const details = Array.from(document.querySelectorAll("details"));
 
 details.forEach((detail) => {
@@ -58,3 +56,19 @@ function openCity(evt, cityName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+
+
+
+// ********** GSAP  ************
+let timeline = gsap.timeline({ defaults: { ease: 'power1.out' } });
+
+timeline.to('.divider', { height: '8rem', duration: 1 });
+timeline.to('.intro-logo', { left: '25%', duration: 1 }, '-=.6');
+timeline.to('.intro-heading', { opacity: '1', left: '55%', duration: 1 }, '-=1');
+timeline.to('.intro-logo', { left: '51%', duration: 1, delay: 1 });
+timeline.to('.intro-heading', { opacity: '0', duration: 1 }, '-=1');
+timeline.to('.divider', { height: '0', duration: 0.5 });
+timeline.to('.loading-screen', { opacity: '0', duration: 0.5, delay: 0.25 });
+timeline.to('.loading-screen', { display: 'none' });
+timeline.to('body', { overflow: 'visible' });
